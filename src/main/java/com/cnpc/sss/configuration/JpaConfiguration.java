ccc1de43ec44f8bc6ce2049aa3bd0af3c9b1efca
fengjieok <luckyjiefeng@163.com>
2018-05-06 22:14:15 +0800

    learnMore-springboot begin

--- src/main/java/com/cnpc/sss/configuration/JpaConfiguration.java
+++ src/main/java/com/cnpc/sss/configuration/JpaConfiguration.java
@@ -1,10 +1,14 @@
 package com.cnpc.sss.configuration;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
@@ -12,9 +16,16 @@ import javax.sql.DataSource;
 @Configuration
 @EnableJpaRepositories("com.cnpc.sss.dao")
 public class JpaConfiguration {
+    @Autowired
+    JpaProperties jpaProperties;
     @Bean
     EntityManagerFactory entityManagerFactory(){
-        return null;
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(this.dataSource());
+        factoryBean.setJpaVendorAdapter(vendorAdapter);
+        factoryBean.setPackagesToScan("com.cnpc.sss.domain");
+        return factoryBean.getObject();
     }
     /**
      * ConfigurationProperties(prefix = "spring.datasource")
