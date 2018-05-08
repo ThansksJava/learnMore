6e39a2ad2f55e9589e07b1d819f7297a3012451f
fengjieok <luckyjiefeng@163.com>
2018-05-08 22:17:17 +0800

    spring security

--- src/main/java/com/cnpc/sss/dao/PersonReposity.java
+++ src/main/java/com/cnpc/sss/dao/PersonReposity.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 import java.util.List;
