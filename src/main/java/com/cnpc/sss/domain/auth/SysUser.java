dd92181f0b1da2128f577de3d0c993c61f614493
fengjieok <luckyjiefeng@163.com>
2018-05-09 14:36:30 +0800

    角色用户搭建，注意实体类中属性注解的位置

---
+++ src/main/java/com/cnpc/sss/domain/auth/SysUser.java
@@ -0,0 +1,96 @@
+package com.cnpc.sss.domain.auth;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @Author fengjie
+ * @Description
+ * @Date Created in 2018/5/9
+ * @Time 14:05
+ */
+@Entity
+public class SysUser implements UserDetails {
+    /**
+     * 注解要么全部打在属性上，要么全部打在get上，不能混用
+     * MappingException: Could not determine type for: java.util.Collection, at table: sys_user, for columns
+     */
+    @Id
+    @GenericGenerator(strategy = "uuid",name="pk_user")
+    @GeneratedValue(generator = "pk_user")
+    private String id;
+    private String username;
+    private String password;
+    @ManyToMany(cascade = {CascadeType.REFRESH},fetch = FetchType.EAGER)
+    private List<SysRole> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> auths = new ArrayList<>();
+        List<SysRole> roles = this.getRoles();
+        for(SysRole role : roles){
+            auths.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return auths;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<SysRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<SysRole> roles) {
+        this.roles = roles;
+    }
+}
