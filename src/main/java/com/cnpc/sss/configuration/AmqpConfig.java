b4a926e5d7d2d2241e6037c48b77eec3a7c64a8d
fengjieok <luckyjiefeng@163.com>
2018-05-15 11:29:52 +0800

    mq demo

--- src/main/java/com/cnpc/sss/configuration/AmqpConfig.java
+++ src/main/java/com/cnpc/sss/configuration/AmqpConfig.java
@@ -1,33 +1,13 @@
 package com.cnpc.sss.configuration;
 
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AmqpConfig {
-    public static final String EXCHANGE   = "spring-boot-exchange";
-    public static final String ROUTINGKEY = "spring-boot-routingKey";
-
     @Bean
-    public ConnectionFactory connectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-        connectionFactory.setAddresses("140.143.231.143:5672");
-        connectionFactory.setUsername("guest");
-        connectionFactory.setPassword("guest");
-        connectionFactory.setVirtualHost("/");
-        connectionFactory.setPublisherConfirms(true); //必须要设置
-        return connectionFactory;
-    }
-    @Bean
-    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    //必须是prototype类型
-    public RabbitTemplate rabbitTemplate() {
-        RabbitTemplate template = new RabbitTemplate(connectionFactory());
-        return template;
+    public Queue queue() {
+        return new Queue("queue");
     }
 }
---
+++ src/test/java/RabbitMQTest.java
@@ -0,0 +1,25 @@
+import com.cnpc.sss.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author fengjie
+ * @Description
+ * @Date Created in 2018/5/15
+ * @Time 9:58
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class RabbitMQTest {
+    @Autowired
+    private AmqpTemplate template;
+    @Test
+    public void testSend(){
+        template.convertAndSend("queue","hello world");
+    }
+
+}
