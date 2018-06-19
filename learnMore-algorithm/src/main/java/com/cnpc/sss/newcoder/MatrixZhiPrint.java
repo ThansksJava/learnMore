48ed63159b2ef7b821b8884e277a55b1f6c6377e
fengjieok <luckyjiefeng@163.com>
2018-06-19 11:23:21 +0800

    之字打印

--- learnMore-algorithm/src/main/java/com/cnpc/sss/newcoder/MatrixZhiPrint.java
+++ learnMore-algorithm/src/main/java/com/cnpc/sss/newcoder/MatrixZhiPrint.java
@@ -7,24 +7,40 @@ package com.cnpc.sss.newcoder;
  * @Time 9:39
  */
 public class MatrixZhiPrint {
-    public static void print(int [][] m,int a,int b,int c,int d){
-        while(a++ < m.length )
-        printDiagonalLine(m, a, b, c, d);
+    public static void print(int [][] m){
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        /*设置true表示从上向下打印*/
+        boolean flag = true;
+        int row = m.length-1;
+        int column = m[0].length-1;
+        while(a != row+1){
+            printDiagonalLine(m,a,b,c,d,flag);
+            /*如果上坐标到了最最右边那么就往下移动*/
+            a = b == column ? a + 1 : a;
+            b = b == column ? b : b + 1;
+            d = c == row ? d + 1 : d;
+            c = c == row ? c : c + 1;
+            flag = !flag;
+        }
 
     }
-    public static void printDiagonalLine(int [][] m,int a,int b,int c,int d){
-        while(a >= c && b <= d){
-            System.out.print(m[a--][b++]);
+    public static void printDiagonalLine(int [][] m,int a,int b,int c,int d,boolean flag){
+        if (flag) {
+            while (a != c + 1) {
+                System.out.print(m[a++][b--] + " ");
+            }
+        } else {
+            while (c != a - 1) {
+                System.out.print(m[c--][d++] + " ");
+            }
         }
     }
 
     public static void main(String[] args) {
-        int [][] a = new int [4][4];
-        for(int i = 0;i < 4;i++){
-            for(int j = 0;j < 4;j++){
-                a[i][j] = i+j;
-            }
-        }
-        printDiagonalLine(a,0,0,0,0);
+        int [][] a = {{1,4,5,7},{2,6,9,1},{3,8,2,5},{7,9,4,3}};
+        print(a);
     }
 }
