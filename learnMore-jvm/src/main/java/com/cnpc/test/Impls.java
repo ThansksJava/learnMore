b59741e3ddfe015d46fa75c1ebabaa943abe32a8
fengjie <luckyjiefeng@163.com>
2018-10-28 09:58:30 +0800

    一些学习的东西

---
+++ learnMore-jvm/src/main/java/com/cnpc/test/Impls.java
@@ -0,0 +1,14 @@
+package com.cnpc.test;
+
+/**
+ * @Author fengjie
+ * @Description
+ * @Date Created in 2018/10/26
+ * @Time 10:13
+ */
+public class Impls implements Interfaces {
+    @Override
+    public void absMethod() {
+
+    }
+}
---
+++ learnMore-jvm/src/main/java/com/cnpc/test/Interfaces.java
@@ -0,0 +1,11 @@
+package com.cnpc.test;
+
+/**
+ * @Author fengjie
+ * @Description
+ * @Date Created in 2018/10/26
+ * @Time 10:13
+ */
+public interface Interfaces {
+    void absMethod();
+}
