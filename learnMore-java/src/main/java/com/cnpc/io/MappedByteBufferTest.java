bb2ecf25aa46890c2ef76f09d49c8881013ce45a
fengjie <luckyjiefeng@163.com>
2018-11-29 09:38:25 +0800

    modify class description

--- learnMore-java/src/main/java/com/cnpc/io/MappedByteBufferTest.java
+++ learnMore-java/src/main/java/com/cnpc/io/MappedByteBufferTest.java
@@ -2,17 +2,14 @@ package com.cnpc.io;
 
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 /**
- * @Author fengjie
- * @Description
- * @Date Created in 2018/11/28
- * @Time 14:50
+ * @author fengjie
+ * @date Created in 2018/11/28
  */
 public class MappedByteBufferTest {
     @Test
