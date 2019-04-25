98675d9a5eda1f5f6d979c0f47cf71a45c642256
fengjie <luckyjiefeng@163.com>
2019-04-25 23:17:18 +0800

    InterruptDemo

---
+++ learnMore-java/src/main/java/com/cnpc/multithread/InterruptDemo.java
@@ -0,0 +1,23 @@
+package com.cnpc.multithread;
+
+/**
+ * @author fengjie
+ * @version 1.0
+ * @date Created in 2019/4/25 23:08
+ */
+public class InterruptDemo {
+    public static void main(String[] args) {
+        new Thread(()->{
+            while(!Thread.interrupted()){
+                System.out.println("检测到中断标志结束线程运行");break;
+            }
+            Thread.currentThread().isInterrupted();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Thread.currentThread().interrupt();
+        }).start();
+    }
+}
