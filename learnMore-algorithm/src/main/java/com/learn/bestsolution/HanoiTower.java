2ab8800dce1a2a809b0cc2decdb95f985355dccb
fengjie01@cnpc.com.cn <Fj1994lucky>
2019-06-27 17:24:39 +0800

    汉诺塔未完

---
+++ learnMore-algorithm/src/main/java/com/learn/bestsolution/HanoiTower.java
@@ -0,0 +1,14 @@
+package com.learn.bestsolution;
+
+/**
+ * 汉诺塔
+ * @author fengjie
+ * @version 1.0
+ * @date Created in 2019/6/26 15:46
+ */
+public class HanoiTower {
+    public int hanoiProblen1(int num,String left,String mid,String right){
+
+        return 0;
+    }
+}
