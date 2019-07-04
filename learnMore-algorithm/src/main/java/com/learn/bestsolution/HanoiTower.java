3e334edfedeefcc66a8536f19e99f8928392d8a9
fengjie01@cnpc.com.cn <Fj1994lucky>
2019-07-04 14:17:27 +0800

    汉诺塔

--- learnMore-algorithm/src/main/java/com/learn/bestsolution/HanoiTower.java
+++ learnMore-algorithm/src/main/java/com/learn/bestsolution/HanoiTower.java
@@ -7,8 +7,33 @@ package com.learn.bestsolution;
  * @date Created in 2019/6/26 15:46
  */
 public class HanoiTower {
-    public int hanoiProblen1(int num,String left,String mid,String right){
+    public static void hanoiProblem1(int num){
+        if(num > 0){
+            func(num,"left","mid","right");
+        }
+    }
 
-        return 0;
+    public static void func(int n,String from,String mid,String to){
+        if(n == 1){
+            System.out.println("move from "+from+" to "+to);
+        }else{
+            //把最底下一层上面的所有从 左 -> 中
+            func(n-1,from,to,mid);
+            //把最后一层从 左 -> 右
+            func(1,from,mid,to);
+            //把其他从 中 -> 右
+            func(n-1,mid,from,to);
+        }
+    }
+
+    public static void main(String[] args) {
+        hanoiProblem1(1);
+        System.out.println("====");
+        hanoiProblem1(2);
+        System.out.println("====");
+        hanoiProblem1(3);
     }
 }
+
+
+
