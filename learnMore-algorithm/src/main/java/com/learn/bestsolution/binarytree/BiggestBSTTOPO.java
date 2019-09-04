21f4923439710b25e1516dadacc0f8f1a6c04224
fengjie <luckyjiefeng@163.com>
2019-09-04 09:30:26 +0800

    二叉树中符合BST的最大拓扑结构

--- learnMore-algorithm/src/main/java/com/learn/bestsolution/binarytree/BiggestBSTTOPO.java
+++ learnMore-algorithm/src/main/java/com/learn/bestsolution/binarytree/BiggestBSTTOPO.java
@@ -1,7 +1,7 @@
 package com.learn.bestsolution.binarytree;
 
 /**
- * 二叉树符合拓扑结构的最大结构
+ * 二叉树中符合BST的最大拓扑结构
  * @author fengjie
  * @version 1.0
  * @date Created in 2019/9/2
@@ -24,11 +24,11 @@ public class BiggestBSTTOPO {
     }
 
     /**
-     * 是否是拓扑结构
+     *
      * @param h 当前树头结点
      * @param n 遍历到的子节点
      * @param value n的值
-     * @return 结果是否是拓扑
+     * @return 结果是否是符合BST
      */
     public static boolean isBSTNode(Node h,Node n,int value){
         if(h == null){
@@ -38,5 +38,25 @@ public class BiggestBSTTOPO {
             return true;
         }
         return isBSTNode(h.value > value?h.left:h.right,n,value);
+    }
+
+    public static void main(String[] args) {
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+//        Node n4 = new Node(4);
+//        Node n5 = new Node(5);
+//        Node n6 = new Node(6);
+//        Node n7 = new Node(7);
+
+        n1.left = n2;
+        n1.right = n3;
+//        n2.left = n4;
+//        n2.right = n5;
+//        n3.left = n6;
+//        n3.right = n7;
+
+        int x = bstTopoSize(n1);
+        System.out.println(x);
     }
 }
