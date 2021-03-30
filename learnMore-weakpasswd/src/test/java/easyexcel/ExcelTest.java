c1c8fcd11e65c8730051a3ab95c3b142e9f0c134
FengJie <Jie.Feng@unisys.com>
2021-03-30 16:10:40 +0800

    fuck the easy excel

---
+++ learnMore-weakpasswd/src/test/java/easyexcel/ExcelTest.java
@@ -0,0 +1,46 @@
+package easyexcel;
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author fengjie
+ * @version 1.0
+ * @date Created in 2021/3/30
+ */
+public class ExcelTest {
+    @Test
+    public void simpleWrite() {
+        // 写法2
+        String fileName = TestFileUtil.getPath() + "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写
+        ExcelWriter excelWriter = null;
+        try {
+            excelWriter = EasyExcel.write(fileName, DemoData.class).build();
+            WriteSheet writeSheet = EasyExcel.writerSheet("模板").build();
+            excelWriter.write(data(), writeSheet);
+        } finally {
+            // 千万别忘记finish 会帮忙关闭流
+            if (excelWriter != null) {
+                excelWriter.finish();
+            }
+        }
+    }
+    private List<DemoData> data() {
+        List<DemoData> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            DemoData data = new DemoData();
+            data.setString("字符串" + i);
+            data.setDate(new Date());
+            data.setDoubleData(0.56);
+            list.add(data);
+        }
+        return list;
+    }
+}
