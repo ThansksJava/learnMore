cd7601795485aa883b3f399a52748d821ca50cdb
fengjie <luckyjiefeng@163.com>
2021-04-06 21:51:00 +0800

    clear springboot module

--- learnMore-springboot/src/main/java/com/learn/Application.java
+++ learnMore-springboot/src/main/java/com/learn/Application.java
@@ -4,23 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * EnableCaching：开启缓存支持
  */
 @SpringBootApplication
-@EnableCaching
-@EnableConfigurationProperties
-//@MapperScan("com.learn.sss.mybatis.dao")
 public class Application extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
     }
+
 }
