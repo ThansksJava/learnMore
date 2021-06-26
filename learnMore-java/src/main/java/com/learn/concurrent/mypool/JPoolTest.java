cd2965cbacb9ad474edc3d01bd0785d5d7652608
fengjie <luckyjiefeng@163.com>
2021-06-26 10:40:54 +0800

    learn thread pool:simplest version 1

--- learnMore-java/src/main/java/com/learn/concurrent/mypool/JPoolTest.java
+++ learnMore-java/src/main/java/com/learn/concurrent/mypool/JPoolTest.java
@@ -2,9 +2,9 @@ package com.learn.concurrent.mypool;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.TimeUnit;
-
 /**
+ * this version only can submit a task and create a thread to execute it,one task one thread,
+ * if the thread num is less than core thread size
  * @author fengjie
  * @version 1.0
  * @date 2021/6/26
